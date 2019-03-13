361eee98d9b0f9e2ce9df05f12daeb619d1ee820
Siddharth Shivkumar <siddharth2798@rediffmail.com>
2019-03-13 10:55:01 +0530

    Fixed Metamask multiple vote registering

--- src/js/app.js
+++ src/js/app.js
@@ -73,14 +73,21 @@ App = {
       electionInstance = instance;
       return electionInstance.candidatesCount();
     }).then(function(candidatesCount) {
+
+       // Store all promised to get candidate info
+      const promises = [];
+      for (var i = 1; i <= candidatesCount; i++) {
+        promises.push(electionInstance.candidates(i));
+      }
+      // Once all candidates are received, add to dom
+      Promise.all(promises).then((candidates) => {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
       var candidatesSelect = $('#candidatesSelect');
       candidatesSelect.empty();
 
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
+       candidates.forEach(candidate => {
           var id = candidate[0];
           var name = candidate[1];
           var voteCount = candidate[2];
@@ -92,8 +99,9 @@ App = {
           // Render candidate ballot option
           var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
           candidatesSelect.append(candidateOption);
+        })
         });
-      }
+      
       return electionInstance.voters(App.account);
     }).then(function(hasVoted) {
       // Do not allow a user to vote
